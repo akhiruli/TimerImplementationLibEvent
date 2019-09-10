14cad0d4f23a62697d1be8e4f8db936d78f88bf9
akhiruli <54841121+akhiruli@users.noreply.github.com>
2019-09-10 19:41:24 +0530

    Update EventImpl.h

--- include/EventImpl.h
+++ include/EventImpl.h
@@ -1,18 +1,8 @@
-/*
- * Copyright (C) Verizon, 2013
- *
- * This unpublished material is proprietary to Verizon.  All rights reserved.
- * The methods and techniques described herein are considered trade secrets
- * and/or confidential. Reproduction or distribution, in whole or in part,
- * is forbidden except by express written permission of Verizon.
- */
 
 //! @file   EventImpl.h 
 //! @brief  
 //! @author Akhirul Islam
-//! @date   Jan, 2017
-//!
-//! 
+
 #ifndef _EVENT_H_
 #define _EVENT_H_
 #include <event2/event.h>
